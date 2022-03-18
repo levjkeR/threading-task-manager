435a0669dd084095ccfb5972d0cc1d15a9f60016
levjkeR <levjke7@gmail.com>
2022-03-18 10:38:53 +0300

    Hot Fix

--- manager.py
+++ manager.py
@@ -1,5 +1,3 @@
-import datetime
-import threading
 from threading import Thread, Event
 import time
 import itertools
@@ -7,34 +5,14 @@ from string import ascii_letters
 from queue import Queue
 from pynput import keyboard
 
-# def on_press(key):
-#     print('Key %s pressed' % key)
-#
-# def on_release(key):
-#     print('Key %s released' %key)
-#
-# listener = keyboard.Listener(
-#     on_press=on_press,
-#     on_release=on_release)
-# listener.start()
-
-MIN_LEN = 5
+MIN_LEN = 3
 MAX_LEN = 26
 QUANTUM = 450000  # nanosecond
-TICK = QUANTUM * 8000
-WORDS = ("BEttA", "glADE", "BrUTE")
+TICK = QUANTUM * 15
+WORDS = ("BEtt", "glAD", "BrUT")
 
 ready_queue = Queue(maxsize=3)
-wait_queue = Queue(maxsize=3)
-block_event = Event()
-
-
-def on_activate():
-    print('Global hotkey activated!')
-
-
-def for_canonical(f):
-    return lambda k: f(listener.canonical(k))
+wait_queue = Queue(maxsize=1)
 
 
 class BruteThread(Thread):
@@ -45,7 +23,6 @@ class BruteThread(Thread):
         self.__is_ready = False
         self.__count = 0
         self.__pause_event = Event()
-        # self.__pause_event.set()
 
     def _simple_brute(self):
         for repeat in range(MIN_LEN, MAX_LEN):
@@ -58,21 +35,22 @@ class BruteThread(Thread):
         return None
 
     def run(self):
-        print(f"Thread '{self.name}' was born!")
         self.__is_ready = True
+        if self.__is_ready:
+            print(f"Thread '{self.name}' was born!")
         if self._simple_brute() is not None:
             print(f"Thread '{self.name}' successfully completed! Word: {self.word} Combinations: {self.__count}")
         else:
             print(f"Thread '{self.name}' unsuccessfully finished! Word: {self.word} Combinations: {self.__count}")
+        self.__is_ready = False
+        return
 
     def pause(self):
         if self.__pause_event.is_set():
-            print(f"Thread '{self.name}' paused")
             self.__pause_event.clear()
 
     def resume(self):
         if not self.__pause_event.is_set():
-            print(f"Thread '{self.name}' resumed")
             self.__pause_event.set()
 
     def is_ready(self):
@@ -87,80 +65,71 @@ class Helper:
         self.__active_thread = None
 
     def set_active_thread(self, thread):
-        self.active_thread = thread
+        self.__active_thread = thread
 
     def get_active_thread(self):
-        return self.active_thread
+        return self.__active_thread
+
+    def wait_timer_or_event(self):
+        start = time.time_ns()
+        while self.__active_thread.is_ready():
+            if time.time_ns() - start >= TICK:
+                ready_queue.put(self.__active_thread)
+                break
+        return
 
 
 threads = [BruteThread("Foo()", WORDS[0]), BruteThread("Bar()", WORDS[1]), BruteThread("Baz()", WORDS[2])]
 
-
-def wait_for_tick_expire(thread):
-    start = time.time_ns()
-    while time.time_ns() - start < TICK and thread.is_ready():
-        pass
-    return
-
-
-# Отправление/возвращение потока в/из нижней очереди
-
-
 Lllecmepka = Helper()
 
 
-
-def on_space():
+def on_hotkey():
     if wait_queue.qsize() >= 1:
-        thread = wait_queue.get()
-        thread.unready()
-        print("Ожидание: ", [i.name for i in wait_queue.queue])
-        print("---------->")
-        ready_queue.put(thread)
-        print("Готовность: ", [i.name for i in ready_queue.queue])
+        active_thread = wait_queue.get()
+        active_thread.unready()
+        ready_queue.put(active_thread)
+        print("Хоткей активирован, отправлен на готовность ", active_thread.name)
     else:
         if Lllecmepka.get_active_thread() is not None:
-            thread = Lllecmepka.get_active_thread()
-            thread.pause()
-            thread.unready()
-            wait_queue.put(thread)
-            print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-            print("Готовность: ", [i.name for i in ready_queue.queue])
-            print("Ожидание: ", [i.name for i in wait_queue.queue])
-            print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+            active_thread = Lllecmepka.get_active_thread()
+            active_thread.pause()
+            active_thread.unready()
+            wait_queue.put(active_thread)
+            print("Хоткей активирован, ожидает ", active_thread)
     return
+
+
+def for_canonical(f):
+    return lambda k: f(listener.canonical(k))
 
 
 hotkey = keyboard.HotKey(
     keyboard.HotKey.parse('<ctrl>+<alt>+k'),
-    on_activate=on_space)
+    on_activate=on_hotkey)
 
 listener = keyboard.Listener(
     on_press=for_canonical(hotkey.press),
     on_release=for_canonical(hotkey.release))
+
 listener.start()
+
 # Порождение
 [item.start() for item in threads]
 
-# Заполнение очереди живыми и готовыми потоками
+# Заполнение очереди готовыми потоками
 for thread in threads:
-    print(2)
-    if thread.is_ready() and thread.is_alive():
+    if thread.is_ready():
         ready_queue.put(thread)
 
 # Основная логика программы
-while True:
-    print("_________________________________________________________")
-    print("Готовность: ", [i.name for i in ready_queue.queue])
-    print("Ожидание: ", [i.name for i in wait_queue.queue])
-    print()
+while ready_queue.qsize() or wait_queue.qsize():
     active_thread = ready_queue.get()
     active_thread.resume()
     Lllecmepka.set_active_thread(active_thread)
-    wait_for_tick_expire(active_thread)
+    Lllecmepka.wait_timer_or_event()
     active_thread.pause()
     Lllecmepka.set_active_thread(None)
-    if active_thread.is_ready():
-        ready_queue.put(active_thread)
-    # ready_queue.put(active_thread)
-
+listener.stop()
+for thread in threads:
+    thread.join()
